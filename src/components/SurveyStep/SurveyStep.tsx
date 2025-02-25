2810d00bf5ced07d209baa2bc5b34f0dd1ff7950
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2025-02-26 00:25:38 +0200

    start button added

---
+++ src/components/SurveyStep/SurveyStep.tsx
@@ -0,0 +1,9 @@
+const SurveyStep = ({ stepName }: { stepName: string }) => {
+    return (
+        <div>
+            <h1>Survey Step {stepName}</h1>
+        </div>
+    );
+};
+
+export default SurveyStep;
