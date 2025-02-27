ea320cf9d96782bfd0e11010d01071f559c7c3a4
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2025-02-27 03:54:19 +0200

    persistor added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import surveyReducer from './surveySlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, surveyReducer);
 
 export const store = configureStore({
   reducer: {
-    survey: surveyReducer,
+    survey: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import { Provider } from 'react-redux';
-import { store } from './index';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './index';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
